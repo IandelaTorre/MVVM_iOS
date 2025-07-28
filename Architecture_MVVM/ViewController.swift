e6d662f66c5ebd915ea9f0176caeba15b470f9eb
Ian Perez <ianaxelperezdelatorre@gmail.com>
2025-07-27 22:52:14 -0600

    Initial Commit

---
+++ Architecture_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture_MVVM
+//
+//  Created by Ian Axel Perez de la Torre on 27/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
